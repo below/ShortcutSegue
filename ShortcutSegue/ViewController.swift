6246f013d2ef8b07e3b37a60831b1245615f75aa
Alexander von Below <Alex@vonBelow.Com>
2018-04-24 12:05:47 +0200

    Initial Commit

---
+++ ShortcutSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShortcutSegue
+//
+//  Created by Alexander v. Below on 24.04.18.
+//  Copyright © 2018 Deutsche Telekom AG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
